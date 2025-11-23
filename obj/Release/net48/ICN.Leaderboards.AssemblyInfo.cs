27be575ad32b403e7e7f53fa2ded330844d79967
IconicPlugins <watsapol.t66@gmail.com>
2025-11-23 23:51:36 +0700

    idk

--- obj/Release/net48/ICN.Leaderboards.AssemblyInfo.cs
+++ obj/Release/net48/ICN.Leaderboards.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Leaderboards plugin for Unturned")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.2.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.2.0+9ef1b90f77e3751966a9d9684a79c0e4843691ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.2.0+2c5b10215527410952cd5b96cfaa05fe15e26c74")]
 [assembly: System.Reflection.AssemblyProductAttribute("ICN.Leaderboards")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ICN.Leaderboards")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.2.0.0")]
